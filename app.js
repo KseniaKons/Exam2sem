cf78349e45099e80b6ca7fb2c9da41dbfb853eab
Ksenia <ksuha280502@gmail.com>
2022-06-29 13:54:38 +0300

    2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import supabaseService from "./supabaseService.js";
 
 const app = express();
 
@@ -6,6 +7,40 @@ const PUBLIC_DIR = "public";
 app.use(express.static(PUBLIC_DIR));
 
 app.get("", (req, res) => {
+
+});
+
+app.post("/add", (req, res) => {
+
+    console.log("metod: post");
+    console.log("url: /add");
+    
+
+    let model = {
+        name: req.body.name,
+        description: req.body.description,
+    };
+
+    let data = supabaseService.addModel(model);
+
+    if (!data) {
+        let error = {
+            status: "error",
+            message: "Ошибка при добавлении в базу данных",
+        };
+        res.statusCode = 400;
+        res.send(error);
+        return;
+    }
+
+
+    let answer = {
+        status: "ok",
+        data
+    }
+
+    res.statusCode = 200;
+    res.send(answer);
 
 });
 
